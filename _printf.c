ab4206fa8ae158fbf9abe720751747c0fa72373c
Your Name <you@example.com>
2023-09-17 07:00:42 -0700

    other one

--- _printf.c
+++ _printf.c
@@ -2,46 +2,50 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * _printf - Produces output according to a format.
+ * @format: A character string containing format specifiers.
+ * @...: Additional arguments corresponding to format specifiers.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-    int count = 0;
+	int count = 0;
+	va_list args;
+	va_start(args, format);
 
-    va_list args;
-    va_start(args, format);
-
-    while (*format)
-    {
-   
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-                break; 
-            else if (*format == 'c')
-            {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str)
-                {
-                    while (*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                }
-                else
-                {
-                   
-                    printf("(null)");
-                    count += 6;
-                }
-	}
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			else if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str)
+				{
+					while (*str)
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+				}
+				else
+				{
+					printf("(null)");
+					count += 6;
+				}
+			}
 			else if (*format == '%')
 			{
 				putchar('%');
@@ -49,7 +53,6 @@ int _printf(const char *format, ...)
 			}
 			else
 			{
-
 				putchar('%');
 				putchar(*format);
 				count += 2;
@@ -57,14 +60,11 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-
 			putchar(*format);
 			count++;
 		}
-
 		format++; 
 	}
-
 	va_end(args); 
 	return count;
 }
